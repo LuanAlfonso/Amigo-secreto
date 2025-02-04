bc24da93a299f654a4b40cc4df2bde6afbd1563f
LuanAlfonso <luanalfonso5@gmail.com>
2025-02-04 16:19:04 -0300

    Update app.js
    adicionando o codigo todo

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
 
+function adicionarAmigos() {
+    let input = document.getElementById("nomeAmigo");
+    let nome = input.value.trim();
+    if (nome == "") {
+        alert("Digite um nome primeiro");
+        return;
+    }
+    amigos.push(nome);
+    input.value = "";
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+    amigos.forEach((amigo) => {
+        let item = document.createElement("li");
+        item.textContent = amigo;
+        lista.appendChild(item);
+    });
+    
+}
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        alert("A lista de amigos está vazia.");
+        return;
+    }
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado]; 
+    document.getElementById("resultadoSorteio").innerHTML = `O amigo secreto sorteado foi: ${amigoSorteado}`;
+}
 
